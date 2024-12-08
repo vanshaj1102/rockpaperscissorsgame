0500d7dc1d6ac13d23d4bd3ce4e73b100f18bf43
vanshaj1102 <126093498+vanshaj1102@users.noreply.github.com>
2024-12-08 20:12:32 +0530

    Update codingpart.py

--- codingpart.py
+++ codingpart.py
@@ -30,9 +30,6 @@ scissors = '''
 game_images = [rock, paper, scissors]
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-# Note: it's worth checking if the user has made a valid choice before the next line of code.
-# If the user typed somthing other than 0, 1 or 2 the next line will give you an error.
-# You could for example write:
 if user_choice >= 0 and user_choice <= 2:
     print(game_images[user_choice])
 
